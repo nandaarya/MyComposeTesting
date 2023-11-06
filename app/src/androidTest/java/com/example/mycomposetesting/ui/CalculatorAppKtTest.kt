d16877853a93c507ed667224bd484588dee4ab66
Nanda Arya Putra <nandaarya404@gmail.com>
2023-11-06 14:40:43 +0700

    Add negative case testing

--- app/src/androidTest/java/com/example/mycomposetesting/ui/CalculatorAppKtTest.kt
+++ app/src/androidTest/java/com/example/mycomposetesting/ui/CalculatorAppKtTest.kt
@@ -1,6 +1,7 @@
 package com.example.mycomposetesting.ui
 
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertHasNoClickAction
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -32,7 +33,20 @@ class CalculatorAppTest {
             .performTextInput("4")
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count))
             .performClick()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count), useUnmergedTree = true)
+            .assertHasNoClickAction()
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.result, 12.0))
+            .assertExists()
+    }
+    @Test
+    fun wrong_input_not_calculated() {
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("..3")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.result, 0.0))
             .assertExists()
     }
 }
