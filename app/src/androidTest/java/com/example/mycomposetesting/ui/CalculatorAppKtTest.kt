302f64a4cc56f6be53f80c0aec9065d72bf7f785
Nanda Arya Putra <nandaarya404@gmail.com>
2023-11-06 14:37:55 +0700

    change createAndroidRule to using createComposeAndroidRule for best practice

--- app/src/androidTest/java/com/example/mycomposetesting/ui/CalculatorAppKtTest.kt
+++ app/src/androidTest/java/com/example/mycomposetesting/ui/CalculatorAppKtTest.kt
@@ -1,9 +1,11 @@
 package com.example.mycomposetesting.ui
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import com.example.mycomposetesting.R
 import com.example.mycomposetesting.ui.theme.MyComposeTestingTheme
 import org.junit.Before
 import org.junit.Rule
@@ -11,7 +13,7 @@ import org.junit.Test
 
 class CalculatorAppTest {
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Before
     fun setUp() {
@@ -24,9 +26,13 @@ class CalculatorAppTest {
 
     @Test
     fun calculate_area_of_rectangle_correct() {
-        composeTestRule.onNodeWithText("Masukkan panjang").performTextInput("3")
-        composeTestRule.onNodeWithText("Masukkan lebar").performTextInput("4")
-        composeTestRule.onNodeWithText("Hitung!").performClick()
-        composeTestRule.onNodeWithText("Hasil: 12.0").assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("3")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.result, 12.0))
+            .assertExists()
     }
 }
