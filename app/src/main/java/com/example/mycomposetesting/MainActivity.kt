a24a1587f2741a0702350ea27bb1d7e5db298bdc
Nanda Arya Putra <nandaarya404@gmail.com>
2024-04-01 01:38:00 +0700

    TES COMMIT 1

--- app/src/main/java/com/example/mycomposetesting/MainActivity.kt
+++ app/src/main/java/com/example/mycomposetesting/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeTestingTheme {
-                // A surface container using the 'background' color from the theme
+                // TES COMMIT 1
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
